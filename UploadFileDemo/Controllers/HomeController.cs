c038e0144102cf972295dd8c0281ebb45681403f
shenqiangbin <shenqiangbin@163.com>
2017-04-09 16:58:11 +0800

    实现基本的文件上传

---
+++ UploadFileDemo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UploadFileDemo.Models;
+
+namespace UploadFileDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(TestModel model, HttpPostedFileBase file)
+        {
+            if (file == null)
+                ViewBag.Msg = "没有文件";
+
+            var fileName = Path.Combine(Request.MapPath("~/Upload"), Path.GetFileName(file.FileName));
+            try
+            {
+                file.SaveAs(fileName);
+                model.AttachmentPath = "../upload/" + Path.GetFileName(file.FileName);
+                ViewBag.Msg = "上传成功";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Msg = ex.Message;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ UploadFileDemo/Models/TestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace UploadFileDemo.Models
+{
+    public class TestModel
+    {
+        [Display(Name = "标题")]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "内容")]
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+
+        public string AttachmentPath { get; set; }
+    }
+}
